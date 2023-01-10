039660a86b285b097047cedc742bb12fd244ed03
Afrie Irham <afrieirham.work@gmail.com>
2023-01-10 11:19:46 +0800

    create ui mockup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ng-todo';
+  tasks = [
+    { id: 1, title: 'Learn Angular 15', completed: false },
+    { id: 2, title: 'Build todo app', completed: false },
+  ];
 }
