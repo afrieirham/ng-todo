cba5023f30c69870e1d475223da9dfe17a8869c5
Afrie Irham <afrieirham.work@gmail.com>
2023-01-10 12:41:28 +0800

    implement add new task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  task = new FormControl('');
   tasks = [
     { id: 1, title: 'Learn Angular 15', completed: false },
     { id: 2, title: 'Build todo app', completed: false },
   ];
+
+  onSubmit() {
+    if (!this.task.value) {
+      return;
+    }
+
+    this.tasks.push({
+      id: Math.random(),
+      title: this.task.value,
+      completed: true,
+    });
+
+    this.task = new FormControl('');
+  }
 }
